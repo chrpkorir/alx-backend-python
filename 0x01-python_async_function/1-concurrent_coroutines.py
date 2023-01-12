dca473627bbf00c4a4641e33522f95b2596b9b7f
[chrpkorir] <faythkoko@gmail.com>
2023-01-12 16:19:43 +0300

    Fixed syntax errors

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+import asyncio
+from random import random
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    spawn_list = []
+    delayed_list = []
+    for i in range(n):
+        delayed_task = asyncio.create_task(wait_random(max_delay))
+        delayed_task.add_done_callback(lambda x: delayed_list.append(x.result()))
+        spawn_list.append(delayed_task)
+
+    for spawn in spawn_list:
+        await spawn
+
+    return delayed_list
